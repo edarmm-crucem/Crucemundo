9d589ce50fff11cab494d9e23a13bd1ef71fd677
edarmm-crucem <edarmm@gmail.com>
2026-05-07 18:12:36 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1175,7 +1175,7 @@ st.markdown(
 
     .action-box {
         width: 100%;
-        min-height: 40px;
+        min-height: 30px;
         border-radius: 22px;
         padding: 1rem;
         margin-bottom: 0.85rem;
