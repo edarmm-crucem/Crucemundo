7fe33cefe7d1227fafdfaeaba84b6709b114dfde
edarmm-crucem <edarmm@gmail.com>
2026-05-08 20:41:24 +0200

    Update app.py

--- app.py
+++ app.py
@@ -359,18 +359,19 @@ def iniciarproceso(sessiontype, templateid, prefixname, processtitle):
     fechastr = datetime.now().strftime("%Y%m%d-%H%M")
     displayuser = st.session_state.get("displayname", "").strip() or "Sin usuario"
     nombrecopia = f"SESION - {displayuser} - {prefixname} - {fechastr}"
-    copyurl = (
-        f"https://docs.google.com/spreadsheets/d/{templateid}/copy"
-        f"?copyDestination={FOLDERSESIONESID}"
-        f"&title={urllib.parse.quote(nombrecopia)}"
-    )
-    st.session_state.confirmstate = "step1"
-    st.session_state.sessiontype = sessiontype
-    st.session_state.nombrecopia = nombrecopia
-    st.session_state.copyurl = copyurl
-    st.session_state.processtitle = processtitle
-    st.session_state.activepanel = "process"
-    st.rerun()
+copia = copyfiletofolder(
+    templateid,
+    nombrecopia,
+    FOLDERSESIONESID
+)
+
+st.session_state.confirmstate = "done"
+st.session_state.sessiontype = sessiontype
+st.session_state.nombrecopia = nombrecopia
+st.session_state.copyurl = copia.get("webViewLink")
+st.session_state.processtitle = processtitle
+st.session_state.activepanel = "process"
+st.rerun()
 
 
 @st.cache_resource
